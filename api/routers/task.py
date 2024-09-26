b14e3325de6c0041661c6d0de982bc41cdb8d739
lukaku3 <kazuhisa@lazyarea.com>
2024-09-27 05:09:45 +0900

    chapter10

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,16 +10,16 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のToDoタスク")]
 
 
-@router.post("/tasks")
-async def create_tasks():
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+async def create_tasks(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
-@router.put("/tasks/{task_id}")
-async def update_tasks():
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_tasks(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 
-@router.delete("/tasks/{task_id}")
-async def delete_tasks():
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_tasks(task_id: int):
     pass
